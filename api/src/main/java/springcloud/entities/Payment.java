5ab812a0db1045dc754114470190ff9ac7849b5c
waynelai85423 <119561918+waynelai85423@users.noreply.github.com>
2023-06-26 00:45:02 +0800

    2023/06/26

---
+++ api/src/main/java/springcloud/entities/Payment.java
@@ -0,0 +1,19 @@
+package springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import utils.AbstractEntity;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment extends AbstractEntity<String> implements Serializable {
+
+    private String serial;
+
+    private String status;
+}
