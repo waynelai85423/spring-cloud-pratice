149be305674fa051f6e2a45d124efb832c56a60f
waynelai85423 <119561918+waynelai85423@users.noreply.github.com>
2023-07-01 13:52:23 +0800

    2023/06/26

---
+++ utils/utils-jpa/src/main/java/utils/AbstractEntity.java
@@ -0,0 +1,98 @@
+package utils;
+
+import com.google.common.collect.Maps;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import tw.com.softleader.rose.validation.group.OnUpdate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@TypeDef(name = "jsonstring", typeClass = JsonStringType.class)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class AbstractEntity<U extends Serializable> implements Serializable {
+
+    /**
+     * id
+     */
+    @NotNull(groups = OnUpdate.class)
+    @Id
+    @GeneratedValue(generator = "pooled")
+    @GenericGenerator(name = "pooled", strategy = "org.hibernate.id.enhanced.TableGenerator", parameters = {
+            @Parameter(name = "table_name", value = "sequence_pooled"),
+            @Parameter(name = "value_column_name", value = "sequence_next_hi_value"),
+            @Parameter(name = "prefer_entity_table_as_segment_value", value = "true"),
+            @Parameter(name = "optimizer", value = "pooled-lo"),
+            @Parameter(name = "initial_value", value = "100001"),
+            @Parameter(name = "increment_size", value = "100") })
+    @Column(name = "id")
+    protected Long id;
+
+    /**
+     * 建立時間
+     */
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    protected LocalDateTime createdDate;
+
+    /**
+     * 建立人員
+     */
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    protected U createdBy;
+
+    /**
+     * 最後修改時間
+     */
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    protected LocalDateTime lastModifiedDate;
+
+    /**
+     * 最後修改人員
+     */
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    protected U lastModifiedBy;
+
+    /**
+     * 是否刪除
+     */
+    @Column(name = "deleted")
+    protected LocalDateTime deleted;
+
+    /**
+     * 資料儲存的版本
+     */
+    @Version
+    @Column(name = "version")
+    Long version;
+
+    /**
+     * 特殊欄位，使用時需先與各方約定
+     */
+    @Type(type = "jsonstring")
+    @Column(name = "extras", columnDefinition = "varchar(4000)")
+    @Singular
+    private Map<String, String> extras = Maps.newHashMap();
+}
+
