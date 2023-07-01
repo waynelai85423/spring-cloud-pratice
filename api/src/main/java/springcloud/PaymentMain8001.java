7c009a02c0a8d6ff66e32c7dc26ccd40c95009b7
waynelai85423 <119561918+waynelai85423@users.noreply.github.com>
2023-07-01 15:08:38 +0800

    2023/06/26

--- api/src/main/java/springcloud/PaymentMain8001.java
+++ api/src/main/java/springcloud/PaymentMain8001.java
@@ -2,10 +2,13 @@ package springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication
+@EntityScan("springcloud.entities.Payment")
 public class PaymentMain8001 {
+
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
     }
